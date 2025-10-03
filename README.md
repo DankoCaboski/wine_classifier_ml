8e2b624d390885dfd601d25c7f4b54220a83515f
DankoCaboski <caboskidanko@gmail.com>
2025-10-02 22:40:15 -0300

    Feat: reports

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# wine_classifier_ml
+# track gender classifier
+
+This repository represents a study on classifying the gender of music tracks based on their audio features.
+The goal is to build a machine learning model that can accurately predict the gender of a track's artist using various audio characteristics.
+
+The raw data is sourced in [kaggle](https://www.kaggle.com/datasets/joebeachcapital/30000-spotify-songs), which provides a rich set of audio features for each track.
+
+## Reports
+
+All the reports are available in the `reports` folder, within a simple explanation of each step taken during the analysis.
--- RandomForestRegressor.py
+++ RandomForestRegressor.py
@@ -5,6 +5,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import os
 
+testName = input("Nome do teste:")
+testeDescription = input("Descrição do teste:")
+
 # 1. Carregar dataset
 data_path = os.path.join("data", "spotify_songs.csv")
 print("Lendo dataset em:", data_path)
@@ -14,11 +17,11 @@ df = pd.read_csv(data_path, on_bad_lines="skip")
 df.columns = df.columns.str.strip().str.lower()
 print("Colunas detectadas:", df.columns.tolist())
 
-# 2. Features numéricascolumns
+# 2. Features numéricas
 features = [
     "danceability", "energy", "loudness", "speechiness",
     "acousticness", "instrumentalness", "liveness",
-    "tempo", "duration_ms", "track_popularity"
+    "valence", "tempo", "duration_ms", "track_popularity"
 ]
 
 # garantir que só use colunas que existem
@@ -42,7 +45,20 @@ model.fit(X_train, y_train)
 
 # 6. Avaliação
 y_pred = model.predict(X_test)
-print("Matriz de Confusão:")
-print(confusion_matrix(y_test, y_pred))
-print("\nRelatório de Classificação:")
-print(classification_report(y_test, y_pred))
+
+print("Exportando resultados para {}...")
+
+output_dir = os.path.join("reports", testName)
+os.makedirs(output_dir, exist_ok=True)
+
+with open(os.path.join(output_dir, f"{testName}_description.txt"), "w") as f:
+    f.write(testeDescription)
+
+confusion_matrix_ = confusion_matrix(y_test, y_pred)
+classification_report_report_df = pd.DataFrame(confusion_matrix_, index=model.classes_, columns=model.classes_)
+classification_report_report_df.to_csv(os.path.join(output_dir, f"{testName}_confusion_matrix.csv"))
+
+# Use output_dict=True for structured DataFrame
+classification_report_dict = classification_report(y_test, y_pred, output_dict=True)
+classification_report_df = pd.DataFrame(classification_report_dict).transpose()
+classification_report_df.to_csv(os.path.join(output_dir, f"{testName}_classification_report.csv"))
